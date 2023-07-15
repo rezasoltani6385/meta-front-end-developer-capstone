4bff2544e53fb9200be3dda447cd8bef98501dab
Reza Soltani <reza.soltani@gmail.com>
2023-07-15 11:36:15 +0330

    add components to src

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import Header from './Header'
+import Nav from './Nav'
+import Main from './Main'
+import Footer from './Footer'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      HomePage
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
