1f87217f16f968280c8b7e72e3ba8c0add768fcc
Reza Soltani <reza.soltani@gmail.com>
2023-07-30 20:17:30 +0330

    add final modifications

--- src/BookingForm.js
+++ src/BookingForm.js
@@ -1,24 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 
-function BookingForm() {
+function BookingForm(props) {
+    const updateTimes = props.updateTimes
+    const dispatch = props.dispatch
     const [data, setData] = useState({date: '', time: '', guests: '', occasion: ''})
+
+    const handleDateChange = (event) => {
+        const selectedDate = event.target.value; // Get the selected date from the form field
+        const newAvailableTimes = updateTimes(selectedDate);
+        // Dispatch the state change
+        // You may need to add a type property to your action to distinguish this update
+        dispatch({ type: 'UPDATE_AVAILABLE_TIMES', payload: newAvailableTimes });
+    };
 
     const handlesetData =  (evt)=>{
         evt.preventDefault()
         setData({...data, [evt.target.id] : evt.target.value})
     }
+
   return (
     <htmlForm style={{display: 'grid', maxWidth: '200px', gap: '20px'}}>
         <label htmlFor="date">Choose date</label>
-            <input type="date" id="date" onChange={handlesetData}/>
+            <input type="date" id="date" onChange={handleDateChange}/>
         <label htmlFor="time">Choose time</label>
         <select id="time" onChange={handlesetData}>
-            <option>17:00</option>
-            <option>18:00</option>
-            <option>19:00</option>
-            <option>20:00</option>
-            <option>21:00</option>  
-            <option>22:00</option>
+            {updateTimes.map ((updateTime, index)=>(
+                <option key={index}>{updateTime}</option>
+            ))}
         </select>
         <label htmlFor="guests">Number of guests</label>
             <input type="number" placeholder="1" min="1" max="10" id="guests" onChange={handlesetData}/>
--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, {useState, useReducer} from 'react'
+import BookingForm from './BookingForm'
 
+// const initialState = ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+const initialState = fetchAPI(new Date())
+
+
+function timeReducer(state, action) {
+  switch (action.type) {
+    // Add more cases here if needed in the future
+    default:
+      return state;
+  }
+}
 function Main() {
+
+  const [availableTimes, dispatch] = useReducer(timeReducer, initialState);
+  
+  console.log(initialState)
+
+  const updateTimes = () => {
+    // You can implement the logic here to update availableTimes based on the selectedDate
+    // For now, just return the same available times
+    return initialState;
+  };
+
   return (
     <div>
-      
+      <BookingForm updateTimes={updateTimes()} dispatch={dispatch} />
     </div>
   )
 }
